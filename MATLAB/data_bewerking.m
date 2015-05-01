7a5c9351f04b32152169f9445052efc9caa3dc67
mdberkers <mdberkers@gmail.com>
2015-05-01 09:49:11 +0200

    werk aan data_bewerk

--- MATLAB/data_bewerking.m
+++ MATLAB/data_bewerking.m
@@ -13,6 +13,17 @@ elseif(data_t_a(i,3)==999)
 end
 i=i+1;
 end
+k = 2;
+while(k<size(data_t_a,1))
+    if (data_t_a(k,2) < 150)
+        data_t_a = data_t_a(1:k-1,1:end);
+        break;
+       
+    end
+k = k+1;  
+end
+
+
 
 Afstand_links_a     = abs(data_t_a(2:end,3)-data_t_a(2,4));%afstand van 0 laten lopen
 Afstand_rechts_a    = abs(data_t_a(2:end,4)-data_t_a(2,3));
@@ -21,8 +32,8 @@ Afstand = (Afstand_rechts_a /10);%Afstand in meters
 
 Time_a = data_t_a(2:end,7); %tijd vanaf zelfde moment metingen
 
-V = diff(Afstand)./diff(Time_a); %afgeleide
-V = [0;V];
+V = diff(Afstand)./diff(Time_a);
+V = [0;V];%afgeleide
 Time_a = [0;Time_a];
 hold on
 %plot(Time_a(1:end-1),V)%velocity plot.
@@ -35,6 +46,7 @@ while(j<size(data_t_d,1)) % remove incorrect measurements
     
     if(data_t_d(j,4) == 999 && data_t_d(j,3) == 999)
         data_t_d = [data_t_d(1:j-1,1:end); data_t_d(j+1:end,1:end)];
+        j= j-1;
     elseif(data_t_d(j,4) == 999)
         data_t_d(j,4) = data_t_d(j,3);
     elseif(data_t_d(j,3) == 999)
@@ -42,17 +54,17 @@ while(j<size(data_t_d,1)) % remove incorrect measurements
     end
 j=j+1;
 end
-
+data_t_d = data_t_d(7:end,1:end);
 distance_left_d = abs(data_t_d(2:end,3)-data_t_d(2,4));
 distance_right_d = abs(data_t_d(2:end,4)-data_t_d(2,3));
 
 distance_d = (distance_right_d / 10); % Distance in meters
 time_d = data_t_d(2:end,7);
 velocity_d = diff(distance_d)./diff(time_d); % Derivative
-velocity_d = [0;velocity_d];
+velocity_d = [velocity_d];
 time_d = [0;time_d];
 
-plot(distance_d,velocity_d);
+plot(distance_d(1:end-1),velocity_d);
 %TODO
 %Aan de hand van de acceleratie en deceleratie curves intersect punt
 %bepalen en de tijd van dit punt teruggeven
