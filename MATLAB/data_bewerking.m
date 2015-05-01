5962212c0923fe1dd11e2a27caa54a4295d71a05
Thomas Brasser <thomasbrasser@gmail.com>
2015-05-01 10:10:44 +0200

    CLEANED  up layout

--- MATLAB/data_bewerking.m
+++ MATLAB/data_bewerking.m
@@ -2,8 +2,8 @@
 clear all;
 load ('data_f_2.mat');
 
-%Acceleratie data
-i=2;
+%%%% REMOVE INCORRECT AND/OR NOT NEEDED MEASUREMENTS %%%%%
+i=2;j=2;k=2;
 while(i<size(data_t_a,1)) % incorrecte metingen weggooien
     if(data_t_a(i,4)==999 && data_t_a(i,3) == 999)
         data_t_a = [data_t_a(1:i-1,1:end); data_t_a(i+1:end,1:end)];
@@ -16,7 +16,6 @@ while(i<size(data_t_a,1)) % incorrecte metingen weggooien
     end
 end
 
-k = 2;
 while(k<size(data_t_a,1))
     if (data_t_a(k,2) < 150)
         data_t_a = data_t_a(1:k-1,1:end);
@@ -25,6 +24,21 @@ while(k<size(data_t_a,1))
     k = k+1;  
 end
 
+while(j<size(data_t_d,1)) 
+    if(data_t_d(j,4) == 999 && data_t_d(j,3) == 999)
+        data_t_d = [data_t_d(1:j-1,1:end); data_t_d(j+1:end,1:end)];
+        j= j-1;
+    elseif(data_t_d(j,4) == 999)
+        data_t_d(j,4) = data_t_d(j,3);
+    elseif(data_t_d(j,3) == 999)
+        data_t_d(j,3) = data_t_d(j,4);   
+    end
+j=j+1;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%% DISTANCES, TIMES and VELOCITIES %%%%%
 Afstand_links_a     = abs(data_t_a(2:end,3)-data_t_a(2,4));%afstand van 0 laten lopen
 Afstand_rechts_a    = abs(data_t_a(2:end,4)-data_t_a(2,3));
     
@@ -36,20 +50,6 @@ V = diff(Afstand)./diff(Time_a);
 V = [0;V];%afgeleide
 Time_a = [0;Time_a];
 
-%Deceleration
-j=2;
-while(j<size(data_t_d,1)) % remove incorrect measurements
-    
-    if(data_t_d(j,4) == 999 && data_t_d(j,3) == 999)
-        data_t_d = [data_t_d(1:j-1,1:end); data_t_d(j+1:end,1:end)];
-        j= j-1;
-    elseif(data_t_d(j,4) == 999)
-        data_t_d(j,4) = data_t_d(j,3);
-    elseif(data_t_d(j,3) == 999)
-        data_t_d(j,3) = data_t_d(j,4);   
-    end
-j=j+1;
-end
 data_t_d = data_t_d(7:end,1:end);
 distance_left_d = abs(data_t_d(2:end,3)-data_t_d(2,4));
 distance_right_d = abs(data_t_d(2:end,4)-data_t_d(2,3));
@@ -59,7 +59,9 @@ time_d = data_t_d(2:end,7);
 velocity_d = diff(distance_d)./diff(time_d); % Derivative
 velocity_d = [velocity_d];
 time_d = [0;time_d];
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%%%%% FIGURES %%%%%
 figure
 hold on
 plot(Afstand,V);
@@ -67,7 +69,3 @@ plot(distance_d(1:end-1),velocity_d);
 legend('Acceleration','Deceleration');
 xlabel('Distance in meters (m)');
 ylabel('Velocity in meters per second (m/s)');
-%TODO
-%Aan de hand van de acceleratie en deceleratie curves intersect punt
-%bepalen en de tijd van dit punt teruggeven
-
