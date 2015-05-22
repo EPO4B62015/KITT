f5082ee8c3c659824591d60c036fbee8bf1c3b4f
Thomas Brasser <thomasbrasser@gmail.com>
2015-05-22 12:06:33 +0200

    added 5 mic audiodata measurements
    also from other groups, ours is called audiodata.mat.

--- TDOA/datagen.m
+++ TDOA/datagen.m
@@ -5,3 +5,7 @@ Ncodebits = 64;                 % # of bits of code
 
 % TODO:  maximize autocorrelation
 x = sign(randn(Ncodebits,1));   % generate random 64 bit binary code
+x = (x > 0 )+ 0;
+x = x';
+r = conv(x, fliplr(x));
+r0 = r(64);
