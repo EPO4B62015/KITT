e52b9b6300a07cebe936ba184ebc99811f6074e6
Thomas Brasser <thomasbrasser@gmail.com>
2015-05-22 11:14:08 +0200

    autocorrelatioin

--- TDOA/datagen.m
+++ TDOA/datagen.m
@@ -1,4 +1,7 @@
-% Generate codeword
+% Generate codeword of Ncodebits with 'high' autocorrelation
 
 Ncodebits = 64;                 % # of bits of code
+
+
+% TODO:  maximize autocorrelation
 x = sign(randn(Ncodebits,1));   % generate random 64 bit binary code
