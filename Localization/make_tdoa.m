b704a8de5d38b650e387822dc7c2bb4433e8376d
Thomas Brasser <thomasbrasser@gmail.com>
2015-05-29 10:28:24 +0200

    changed x to clearer variable names

--- Localization/make_tdoa.m
+++ Localization/make_tdoa.m
@@ -1,9 +1,9 @@
-function tdoa = make_tdoa(x_matrix_mics, x_location)
+function tdoa = make_tdoa(matrix_mics_position, car_location)
 % Generates test TDOA data
 % input: mic positions, some field position
 % output: TDOA data
-    [row, col] = size(x_matrix_mics);
-    dimensions = length(x_location);
+    [row, col] = size(matrix_mics_position);
+    dimensions = length(car_location);
     if(col ~= dimensions) % check for same dimensions
         error('Mics location dimensions dont match with x_location dimensions');
     end
@@ -12,7 +12,7 @@ function tdoa = make_tdoa(x_matrix_mics, x_location)
     mic1 = 1;
     mic2 = 2;
     for i = 1:elements
-        tdoa(i, 1) = (norm(x_matrix_mics(mic1, 1:col) - x_location) - norm(x_matrix_mics(mic2, 1:col) - x_location));
+        tdoa(i, 1) = (norm(matrix_mics_position(mic1, 1:col) - car_location) - norm(matrix_mics_position(mic2, 1:col) - car_location));
         mic2 = mic2 + 1;
         if(mic2 > row)
             mic1 = mic1 + 1;
