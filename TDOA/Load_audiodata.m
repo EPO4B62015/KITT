99dd44f50919c1495ac1d8ad5b7bb239acf2b69f
mdberkers <mdberkers@gmail.com>
2015-05-29 09:17:21 +0200

    voor testen
    metingen uitlezen

---
+++ TDOA/Load_audiodata.m
@@ -0,0 +1,11 @@
+load ('audiodata.mat')
+meting_1 = RXXr(1,:,:);
+meting_2 = RXXr(2,:,:);
+meting_3 = RXXr(3,:,:);
+meting_4 = RXXr(4,:,:);
+meting_5 = RXXr(5,:,:);
+meting_6 = RXXr(6,:,:);
+meting_7 = RXXr(7,:,:);
+meting_8 = RXXr(8,:,:);
+meting_9 = RXXr(9,:,:);
+meting_10 = RXXr(10,:,:);
--- TDOA/TDOA_5channel/functions/ch5_tdoa.m
+++ TDOA/TDOA_5channel/functions/ch5_tdoa.m
@@ -1,7 +1,5 @@
-%function TDOA = ch5_tdoa(input_matrix)
-load('audiodata.mat')
-input_matrix = RXXr(10,:,:);
-speed_of_sound = 340.29;
+function TDOA = ch5_tdoa(input_matrix)
+speed_of_sound = 340;
 
 data_mic_1 = input_matrix(1,(1:length(input_matrix)),1)';
 data_mic_2 = input_matrix(1,(1:length(input_matrix)),2)';
@@ -16,19 +14,19 @@ data_mic_5 = input_matrix(1,(1:length(input_matrix)),5)';
 [channel_fe_5, channel_mf_5] = channel_estimation_5ch(data_mic_5);
 
 
-position_mic_mf(1) = distance(channel_mf_1, channel_mf_1)
-position_mic_mf(2) = distance(channel_mf_1, channel_mf_2)
-position_mic_mf(3) = distance(channel_mf_1, channel_mf_3)
-position_mic_mf(4) = distance(channel_mf_1, channel_mf_4)
-position_mic_mf(5) = distance(channel_mf_1, channel_mf_5)
+position_mic_mf(1) = distance(channel_mf_1, channel_mf_1);
+position_mic_mf(2) = distance(channel_mf_1, channel_mf_2);
+position_mic_mf(3) = distance(channel_mf_1, channel_mf_3);
+position_mic_mf(4) = distance(channel_mf_1, channel_mf_4);
+position_mic_mf(5) = distance(channel_mf_1, channel_mf_5);
 
-
+TDOA = zeros(5);
 for x = 1:5
     for y = 1:5
         TDOA(x,y) = position_mic_mf(x) - position_mic_mf(y);
     end
 end
-%TDOA = TDOA ./ 48000;
-%TDOA = TDOA .* speed_of_sound
+TDOA = TDOA ./ 48000;
+TDOA = TDOA .* speed_of_sound;
 
-%end
+end
