7f89976d41beeba342e5ddfbda668d904a122b80
Thomas Brasser <thomasbrasser@gmail.com>
2015-05-29 10:29:47 +0200

    more commentary

--- Localization/make_tdoa.m
+++ Localization/make_tdoa.m
@@ -2,8 +2,8 @@ function tdoa = make_tdoa(matrix_mics_position, car_location)
 % Generates test TDOA data
 % input: mic positions, some field position
 % output: TDOA data
-    [row, col] = size(matrix_mics_position);
-    dimensions = length(car_location);
+    [row, col] = size(matrix_mics_position); % number of mics and dimension of their location
+    dimensions = length(car_location); %dimension of car location data
     if(col ~= dimensions) % check for same dimensions
         error('Mics location dimensions dont match with x_location dimensions');
     end
