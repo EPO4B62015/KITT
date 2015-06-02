118387cd9ffe774558d652cafd17d6500767cb82
Thomas Brasser <thomasbrasser@gmail.com>
2015-06-02 10:16:41 +0200

    update beaconsettings file

--- TDOA/BeaconSettings.m
+++ TDOA/BeaconSettings.m
@@ -6,8 +6,8 @@
 %timer_0 / timer_1 > 4 and whole
 %
 
-car_freq = [5 10 20 25 30];
-code_freq = [1 1.5 2 2.5 3 3.5 4 4.5 5];
-repeat_freq = [1 2 3 4 5 6 7 8 9 10];
+car_freq = [5 10 15 20 25 30]; %kHz
+code_freq = [1 1.5 2 2.5 3 3.5 4 4.5 5]; %kHz
+repeat_freq = [1 2 3 4 5 6 7 8 9 10]; %Hz
 
 %code word is random. With good autocorolation
