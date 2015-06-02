39a105ab43206d1c60829b69cfcc4b0bb0e7eb10
Thomas Brasser <thomasbrasser@gmail.com>
2015-06-02 11:16:59 +0200

    framework to measure time to steer
    to reach certain (relative) orientation.

---
+++ steering_measurements/check_times.m
@@ -0,0 +1,53 @@
+% figure out how long steering x degrees takes when driving at different
+% speeds.
+
+% speed and steering settings
+% speeds
+v_fast    = v_neutral + 15;
+v_normal  = v_neutral + 5; 
+v_neutral = 150;
+v_brake   = v-neutral -15;
+% steering
+d_left    = d_neutral - 15;
+d_neutral = 153;
+d_right   = d_neutral + 15;
+
+    % 15 degrees change
+    % 30 degrees change
+
+% 45 degrees change (medium left or medium right)
+drive(v_fast,d_left);
+    % 60 degrees change
+    % 75 degrees change
+
+% 90 degrees change (left or right)
+
+    % 105 degrees change
+    % 120 degrees change
+
+% 135 degrees change
+
+    % 150 degrees change
+    % 165 degrees change
+
+% 180 degrees change (turn around left or right)
+
+    % 195 degrees change
+    % 210 degrees change
+
+% 225 degrees change
+
+    % 240 degrees change
+    % 255 degrees change
+
+% 270 degrees change (3/4 circle left or right)
+
+    % 285 degrees change
+    % 300 degrees change
+
+% 315 degrees change
+
+    % 330 degrees change
+    % 345 degrees change
+
+% 360 degrees change (full circle left or right)
