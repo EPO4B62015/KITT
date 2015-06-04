5eed319810e82b4e2abb29daca0f53d8119d08e9
richarddj <richard.dj@live.nl>
2015-06-04 16:42:29 +0200

    Final Challenge Folder
    Final Challenge map aangemaakt om overzicht te houden en eerste files
    toegevoegd

---
+++ Final Challenge/drive_car.m
@@ -0,0 +1,11 @@
+function drive_car(speedsetting, steersetting, time_to_drive)
+%DRIVE_CAR Summary of this function goes here
+%   Detailed explanation goes here
+
+
+drive(speedsetting, steersetting);
+pause(time_to_drive);
+drive(150, 153);
+
+end
+
