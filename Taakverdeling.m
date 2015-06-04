f54a20e2198fa6550a05148ef7087893cb9d3934
richarddj <richard.dj@live.nl>
2015-06-04 11:51:23 +0200

    Taakverdeling
    Morgen alles klaar zodat we kunnen testen

---
+++ Taakverdeling.m
@@ -0,0 +1,22 @@
+%Controle loop --> Jeroen
+%overzicht
+%Timer functie --> Jeroen
+
+%Voltage bepalen - voor het wegrijden --> Thomas
+
+
+%Positie ophalen ->
+%-> 3x ch5_tdoa --> Martijn, Richard
+%-> definitieve positie = localize(TDOA('s), vorige positie/verwachte,
+%richting) --> Jeroen
+
+%Orientatie(positie vector, vorige orientatie) --> Martijn,Richard
+
+%Planner(Orientatie,definitievepositie, geplandepositie/goal/waypoint) -->
+%Thomas
+    % output: speedsetting, steeringsetting, time to drive
+
+%Aansturen(speedsetting, steeringsetting, time to drive) --> Richard,
+%Martijn
+
+%Grid bijwerken --> Richard/Jeroen
