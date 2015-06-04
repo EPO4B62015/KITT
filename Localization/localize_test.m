6f0c6b0e7c6bfa4bdc490728db248fcc3895f781
Jeroen van Uffelen <jeroenvanuffelen@hotmail.com>
2015-06-04 11:52:27 +0200

    Test voor localize

---
+++ Localization/localize_test.m
@@ -0,0 +1,11 @@
+clear;
+close all;
+load mic_positions
+data = zeros(1, 414);
+for i = 0:413
+    tdoa = make_tdoa(x, [i 105 26]);
+    pos = localize(x, tdoa);
+    data(1,i+1) = pos(1) - i;
+    %data(2,i+1) = pos(3) - 26;
+end
+plot(data);
