940404ba2a39f8851271b6c34247f5fab33880a9
Thomas Brasser <thomasbrasser@gmail.com>
2015-06-05 10:35:51 +0200

    planner
    checks if previously made turn, if true drives for 0.5s in a straight
    line, if false:  checks if it has to make a turn or drive straight. If
    it has to make a turn it does. If it has to drive straight it checks if
    the distance is greater than 1.5m, if it is it will drive for 1 second,
    otherwise it will drive for a time that has to be determined for the
    remaining distance.

--- planner/planner.m
+++ planner/planner.m
@@ -1,10 +1,32 @@
 %Planner
-%input: position(x,y,orientation) , next_position(x,y), 
-delta_x = next_position(1)-position(1);
-delta_y = next_position(2)-position(2);
-if position(orientation) == arctan((next_position(1)-position(1))/(next_position(2)-position(2))) % no steering necessary, so straight line
-    % drive straight line with length pytagoras: length = sqrt((next_position(1)-position(1)² +
-    % route(y)²)
+%input: position(x,y,orientation) ,next_position(x,y), 
+d_x             = next_position(1)-position(1,end);
+d_y             = next_position(2)-position(2,end);
+desired_theta   = arctan2d(d_y/d_x);
+d_theta         = desired_theta-position(3,end);
+distance        = sqrt(d_x^2 + d_y^2);
+
+if madeturn == true
+    time        = 0.5;
+    steer       = 153;
+    madeturn    = false;
 else
-    % make a turn with route(orientation) as the angle
+    if abs(d_theta) <= 5    %straigth
+        if distance <= 1.5;
+            time    = drive(distance);
+            steer   = 153;
+        else                %distance is greater than 1.5m
+            time    = 1;    % return a 1s drive
+            steer   = 153;  % straight
+        end
+    else                    % turn
+        d_theta     = roundn(d_theta,1.5); % round up or down to nearest 15 degrees.
+        madeturn    = true; % make sure that we drive a bit in a straight line next
+        if d_theta <= 0
+            steer = 200;
+        else
+            steer = 100;
+        end
+        time = turn(d_theta);
+    end
 end
--- steering_measurements/check_times.m
+++ steering_measurements/check_times.m
@@ -23,38 +23,28 @@ d_right   = d_neutral + 47;
     % 30 degrees change
 
 % 45 degrees change (medium left or medium right)
-steering_timer(v_fast,d_left,1,0.01);
+steering_timer(155,100,1,0.01);
     % 60 degrees change
     % 75 degrees change
 
 % 90 degrees change (left or right)
-steering_timer(v_fast,d_left,2.0,0.01);
-    % 105 degrees change
-    % 120 degrees change
+%steering_timer(v_fast,d_left,2.0,0.01);
 
 % 135 degrees change
 
-    % 150 degrees change
-    % 165 degrees change
+
 
 % 180 degrees change (turn around left or right)
 
-    % 195 degrees change
-    % 210 degrees change
 
 % 225 degrees change
 
-    % 240 degrees change
-    % 255 degrees change
-
 % 270 degrees change (3/4 circle left or right)
 
-    % 285 degrees change
-    % 300 degrees change
+
 
 % 315 degrees change
 
-    % 330 degrees change
-    % 345 degrees change
+
 
 % 360 degrees change (full circle left or right)
