400219f657fa4574ca9dd5d0b527c222a69addef
richarddj <richard.dj@live.nl>
2015-06-05 10:37:16 +0200

    Update control_loop

--- Final Challenge/TDOA.m
+++ Final Challenge/TDOA.m
@@ -3,7 +3,7 @@ function [ TDOA_data ] = TDOA
 %   TDOA runs the tdoa and returns a column with distances differences
 Fs = 48000;
 Trec = 0.25;
-measured_data = pa_wavrecord(1, 5, Fs*Trec, Fs, 'asio');
+measured_data = pa_wavrecord(3, 7, Fs*Trec, Fs, 'asio');
 [TDOA_matrix, TDOA_data] = ch5_tdoa_final(measured_data);
 end
 
