c9d69c602d02773415db52f6ca72cd451260be4e
richarddj <richard.dj@live.nl>
2015-06-09 09:40:08 +0200

    Updat tdoa FInal

--- Final Challenge/ch5_tdoa_final.m
+++ Final Challenge/ch5_tdoa_final.m
@@ -4,11 +4,11 @@ function [TDOA, TDOA_2] = ch5_tdoa_final(input_matrix)
 speed_of_sound = 340;
 
 %Put the measurement data in seperate variables
-data_mic_1 = input_matrix(1,(1:length(input_matrix)),1)';
-data_mic_2 = input_matrix(1,(1:length(input_matrix)),2)';
-data_mic_3 = input_matrix(1,(1:length(input_matrix)),3)';
-data_mic_4 = input_matrix(1,(1:length(input_matrix)),4)';
-data_mic_5 = input_matrix(1,(1:length(input_matrix)),5)';
+data_mic_1 = input_matrix((1:length(input_matrix)),1);
+data_mic_2 = input_matrix((1:length(input_matrix)),2);
+data_mic_3 = input_matrix((1:length(input_matrix)),3);
+data_mic_4 = input_matrix((1:length(input_matrix)),4);
+data_mic_5 = input_matrix((1:length(input_matrix)),5);
 
 %Calculate the channel estimates for all mics
 [channel_fe_1, channel_mf_1] = channel_estimation_5ch(data_mic_1);
@@ -24,11 +24,11 @@ data_mic_5 = input_matrix(1,(1:length(input_matrix)),5)';
 % position_mic_mf(4) = distance_test(position_mic_mf(1), channel_mf_4);
 % position_mic_mf(5) = distance_test(position_mic_mf(1), channel_mf_5);
 % 
-position_mic_mf(1) = loc_peak_1_test(channel_fe_1);
-position_mic_mf(2) = distance_test(position_mic_mf(1), channel_fe_2);
-position_mic_mf(3) = distance_test(position_mic_mf(1), channel_fe_3);
-position_mic_mf(4) = distance_test(position_mic_mf(1), channel_fe_4);
-position_mic_mf(5) = distance_test(position_mic_mf(1), channel_fe_5);
+position_mic_mf(1) = loc_peak_1_final(channel_fe_1);
+position_mic_mf(2) = distance_final(position_mic_mf(1), channel_fe_2);
+position_mic_mf(3) = distance_final(position_mic_mf(1), channel_fe_3);
+position_mic_mf(4) = distance_final(position_mic_mf(1), channel_fe_4);
+position_mic_mf(5) = distance_final(position_mic_mf(1), channel_fe_5);
 
 %Calculate all the differences in distances
 TDOA = zeros(5);
