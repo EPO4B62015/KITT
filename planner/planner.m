edbcaf2a723f0baad9d9b287e92facf2abcc3df7
richarddj <richard.dj@live.nl>
2015-06-09 12:25:45 +0200

    thomas richard laptop

--- planner/planner.m
+++ planner/planner.m
@@ -9,7 +9,7 @@ d_y             = next_position(2)-position(2,end);
 desired_theta   = atan2d(d_y,d_x);
 d_theta         = desired_theta - position(3,end);
 distance        = sqrt(d_x^2 + d_y^2);
-test_data.dtheta = [test_data.dtheta, d_theta]; 
+%test_data.dtheta = [test_data.dtheta, d_theta]; 
 
 if car.did_turn == true
     time        = 0.5;
@@ -28,7 +28,7 @@ else
         %d_theta     = roundn(d_theta,1.5); % round up or down to nearest 15 degrees.
         car.did_turn    = true; % make sure that we drive a bit in a straight line next
         if d_theta <= 0
-            steer = 200;
+            steer = 197;
         else
             steer = 100;
         end
--- planner/turn.m
+++ planner/turn.m
@@ -5,10 +5,10 @@ function [ time ] = turn(d_theta)
 % Moet nog getweaked / gecalibreerd worden
 % positive d_theta
 if d_theta > 5
-    time = 0.01622222222*d_theta;
+    time = 0.01622222222*d_theta +0.5;
 elseif d_theta < -5
     % negative d_theta 
-    time = -0.02277777777*d_theta;
+    time = -0.0162222222*d_theta +0.5;
 else 
     time = 0;
 end
