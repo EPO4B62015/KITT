7beb1822f01cc8743dd54b9e430147244faa6a9a
Thomas Brasser <thomasbrasser@gmail.com>
2015-06-09 10:36:13 +0200

    simpel linear alles geimplementeerd, voor turn en straight

--- planner/intersect_curves/dist_time_dec.m
+++
@@ -1,9 +0,0 @@
-function [fitresult, gof] = dist_time_dec(distance_d_curve, time_d_curve)
-%% Fit: 'untitled fit 1'.
-[xData, yData] = prepareCurveData( distance_d_curve, time_d_curve );
-
-% Set up fittype and options.
-ft = 'linearinterp';
-
-% Fit model to data.
-[fitresult, gof] = fit( xData, yData, ft, 'Normalize', 'on' );
---
+++ planner/straight.m
@@ -0,0 +1,9 @@
+function [ time ] = straight(distance)
+%TURN Summary of this function goes here
+%   returns time to drive a certain distance lower than 1.5m (for now)
+
+% Moet nog getweaked / gecalibreerd worden
+time = (2/1.5) * distance;
+
+end
+
