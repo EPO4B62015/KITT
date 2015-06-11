dc4a62ae4a64af6a6259ed7bfc27fa3b305a0b97
Thomas Brasser <thomasbrasser@gmail.com>
2015-06-11 16:22:26 +0200

    made d_theta global,  so now it's car.d_theta

--- planner/planner.m
+++ planner/planner.m
@@ -7,16 +7,16 @@ speed           = 158;
 d_x             = next_position(1)-position(1,end);
 d_y             = next_position(2)-position(2,end);
 desired_theta   = atan2d(d_y,d_x);
-d_theta         = desired_theta - position(3,end);
+car.d_theta         = desired_theta - position(3,end);
 distance        = sqrt(d_x^2 + d_y^2);
-%test_data.dtheta = [test_data.dtheta, d_theta]; 
+%test_data.dtheta = [test_data.dtheta, car.d_theta]; 
 
 if car.did_turn == true
     time        = 0.5;
     steer       = 153;
     car.did_turn    = false;
 else
-    if abs(d_theta) <= 5    %straigth
+    if abs(car.d_theta) <= 5    %straigth
         if distance <= 1.5;
             time    = straight(distance);
             steer   = 153;
@@ -25,14 +25,14 @@ else
             steer   = 153;  % straight
         end
     else                    % turn
-        %d_theta     = roundn(d_theta,1.5); % round up or down to nearest 15 degrees.
+        %car.d_theta     = roundn(car.d_theta,1.5); % round up or down to nearest 15 degrees.
         car.did_turn    = true; % make sure that we drive a bit in a straight line next
-        if d_theta <= 0
+        if car.d_theta <= 0
             steer = 197;
         else
             steer = 100;
         end
-        time = turn(d_theta);
+        time = turn(car.d_theta);
     end
 end
 
