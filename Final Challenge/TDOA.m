ed38a1e422f91c471e79a6e5bc97631f2954d582
mdberkers <mdberkers@gmail.com>
2015-06-16 01:00:13 +0200

    removed whitespace for report

--- Final Challenge/TDOA.m
+++ Final Challenge/TDOA.m
@@ -1,12 +1,8 @@
 function [ TDOA_data ] = TDOA
-%TDOA Summary of this function goes here
-%   TDOA runs the tdoa and returns a column with distances differences
-global test_data
-global i_measured
+%TDOA runs the tdoa and returns a column with distances differences
 Fs = 48000;
 Trec = 0.25;
 measured_data = pa_wavrecord(3, 7, Fs*Trec, Fs, 'asio');
-disp('gfjuio');
 [TDOA_matrix, TDOA_data] = ch5_tdoa_final(measured_data);
 end
 
--- Final Challenge/ch5_tdoa_final.m
+++ Final Challenge/ch5_tdoa_final.m
@@ -1,6 +1,6 @@
-%Calculates the TDOA of the 5 channels
-%TEST
+
 function [TDOA, TDOA_2] = ch5_tdoa_final(input_matrix)
+%Calculates the TDOA of the 5 channels
 speed_of_sound = 340;
 
 %Put the measurement data in seperate variables
@@ -17,31 +17,23 @@ data_mic_5 = input_matrix((1:length(input_matrix)),5);
 [channel_fe_4, channel_mf_4] = channel_estimation_5ch(data_mic_4);
 [channel_fe_5, channel_mf_5] = channel_estimation_5ch(data_mic_5);
 
-% % Calculate the differences in arrival times in samples
-% position_mic_mf(1) = loc_peak_1_test(channel_mf_1);
-% position_mic_mf(2) = distance_test(position_mic_mf(1), channel_mf_2);
-% position_mic_mf(3) = distance_test(position_mic_mf(1), channel_mf_3);
-% position_mic_mf(4) = distance_test(position_mic_mf(1), channel_mf_4);
-% position_mic_mf(5) = distance_test(position_mic_mf(1), channel_mf_5);
-% 
-position_mic_mf(1) = loc_peak_1_final(channel_fe_1);
-position_mic_mf(2) = distance_final(position_mic_mf(1), channel_fe_2);
-position_mic_mf(3) = distance_final(position_mic_mf(1), channel_fe_3);
-position_mic_mf(4) = distance_final(position_mic_mf(1), channel_fe_4);
-position_mic_mf(5) = distance_final(position_mic_mf(1), channel_fe_5);
+%detect position of the peak in each channel
+position_mic(1) = loc_peak_1_final(channel_fe_1);
+position_mic(2) = distance_final(position_mic(1), channel_fe_2);
+position_mic(3) = distance_final(position_mic(1), channel_fe_3);
+position_mic(4) = distance_final(position_mic(1), channel_fe_4);
+position_mic(5) = distance_final(position_mic(1), channel_fe_5);
 
 %Calculate all the differences in distances
 TDOA = zeros(5);
 for x = 1:5
     for y = 1:5
-        TDOA(y,x) = position_mic_mf(x) - position_mic_mf(y);
+        TDOA(y,x) = position_mic(x) - position_mic(y);
     end
 end
-
 %Calculate the TDOA in centimers
 TDOA = TDOA ./ 48000;
-TDOA = TDOA .* speed_of_sound*100;
-
+TDOA = TDOA .* speed_of_sound*100; 
 %Make an array from the data for the function localize
 TDOA_2 = [TDOA(2,1);TDOA(3,1);TDOA(4,1);TDOA(5,1);TDOA(3,2);TDOA(4,2);TDOA(5,2);TDOA(4,3);TDOA(5,3);TDOA(5,4)];
 end
