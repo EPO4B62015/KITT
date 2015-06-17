4f2cdce27b1b662b1e247cf3938b21b4cdf85532
Jeroen van Uffelen <jeroenvanuffelen@hotmail.com>
2015-06-17 15:16:49 +0200

    Speciaal voor Thomas

--- Git FINAL test FINAL GIT OPST 2/EPOfigure_testdata.m
+++ Git FINAL test FINAL GIT OPST 2/EPOfigure_testdata.m
@@ -1,8 +1,8 @@
 %Create EPOFIGURE from testdata
 close all;
 %load testdata
-load test2.mat
-post_test = test_data.pos_tdoa;
+load Test3.mat
+post_test = position;
 i = 1;
 EPO4figure; %Load the figure
 EPO4figure.setMicLoc(static_positions.mic_positions/100) %Update Mic Positions
@@ -15,5 +15,5 @@ EPO4figure.setKITT(static_positions.origin(1:2,1)/100, 1);
 for i = 1:m
     
     EPO4figure.setKITT([post_test(1,i)/100 post_test(2,i)/100], 0); %Update car position
-    pause(0.2);
+    pause(0.5);
 end
